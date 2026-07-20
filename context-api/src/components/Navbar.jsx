25d295fc1a0c13a7df97b36b38601a5ab2eba70c
Aditya Shukla <adityaxbuilds@gmail.com>
2026-07-20 11:37:28 +0530

    .

---
+++ context-api/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Nav2 from "./nav2"
+
+const Navbar = () =>{
+    return(
+        <div className="Navbar">
+            <h2>Aditya</h2>
+            <Nav2 />
+        </div>
+    )
+}
+export default Navbar
---
+++ context-api/src/components/nav2.jsx
@@ -0,0 +1,11 @@
+const Nav2 = () => {
+    return (
+
+        <div className="Nav2">
+            <h4>Home</h4>
+            <h4>About</h4>
+            <h4>Contact</h4>
+        </div>
+    )
+}
+export default Nav2
